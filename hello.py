8e6b08e319341a64fb58d214ac0e6dad209f8c94
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2026-03-27 14:14:28 +0000

    Adicionei o Espanhol e Frances

--- hello.py
+++ hello.py
@@ -34,5 +34,9 @@ if current_language == "pt_BR":
     msg = "Ola, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hole, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
 
 print(msg)
