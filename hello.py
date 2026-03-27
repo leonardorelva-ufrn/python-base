e75de4c955db58b3393e05790bdc305d018205bd
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2026-03-27 13:21:24 +0000

    Hello World Multi Linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# Este programa imprime Hello World
-print("Hello, World!")
+Dependendo da lingua configurada no ambiente o programa exibi a mensagem
+correspondente
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+        export LANG=pt_BR
+
+Execulcao:
+
+        python3 hello.py
+        ou
+        ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Leonardo Relva"
+__license__ = "Unlicense"
+
+import os
+
+name = os.getenv('LOGNAME') # Pega os dados ambiente (env) do LOGNAME
+print(f"NAME: {name}")
+
+current_language = os.getenv('LANG')[:5] # Pega os primeiro 5 digitos do LANG
+print(current_language)
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
